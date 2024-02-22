9cfc9d9dc8711dd408429c17d4ad97e49489e59d
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-22 16:00:21 +0500

    Add files via upload

---
+++ 13-transport-array/index1.ts
@@ -0,0 +1,9 @@
+// This program written : Muhammad Javed
+// Date :20.02.2024
+// Description :I am Typescript.
+let  name1:string = "Muhammad Javed";
+console.log("Hello, "+name1);
+let name2 = name1.split(' ');
+console.log(name2);
+let fullName = name2[0] +" "+ name2[1];
+console.log("Full Name is : ",fullName);
