e872421840729b995ab1d56c05b48b852a2e777f
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-22 15:26:57 +0500

    Add files via upload

---
+++ 06-stripping-name/index.js
@@ -0,0 +1,4 @@
+var name1 = '    \n\tJaved store\n\t   ';
+console.log(name1);
+var name2 = name1.trim();
+console.log(name2);
---
+++ 06-stripping-name/index.ts
@@ -0,0 +1,4 @@
+const name1:string='    \n\tJaved store\n\t   ';
+console.log(name1);
+const name2:string=name1.trim();
+console.log(name2);   
