beb9629ff16dacd8a3d5f9b48fca36021c80773d
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-22 16:03:38 +0500

    Add files via upload

---
+++ 14-guest-list/index1.js
@@ -0,0 +1,9 @@
+// This program written : Muhammad Javed
+// Date :20.02.2024
+// Description :I am Typescript.
+var name1 = "Muhammad Javed";
+console.log("Hello, " + name1);
+var name2 = name1.split(' ');
+console.log(name2);
+var fullName = name2[0] + " " + name2[1];
+console.log("Full Name is : ", fullName);
