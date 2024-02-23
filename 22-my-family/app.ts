c65816aab7bb8c36337125b7de67ca40d2ceb3d6
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-23 19:38:39 +0500

    Add files via upload

---
+++ 22-my-family/app.ts
@@ -0,0 +1,15 @@
+
+// Intentional Error: If you haven’t received an array index error 
+// in one of your programs yet, try to make one happen. Change an index 
+// in one of your programs to produce an index error.
+//  Make sure you correct the error before closing the program.
+
+let my_family =['mama','papa', 'brother', 'sister', 'cousin'];
+console.log(my_family[6]);
+// The code above will produce an index error because the array
+//  "my_family" only has five elements with indices from 0 to 4. Trying to
+// access "my_family[6]" will result in an error because there is no element 
+//  at that index. To fix this issue, simply change the number in the square brackets  
+// to a valid index for the array. 
+// it also called international error
+console.log(my_family[3]);
