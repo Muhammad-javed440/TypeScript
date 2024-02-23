fba742d368dc99dc892a151d7e2578357d3cf0e7
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-24 00:38:38 +0500

    Add files via upload

---
+++ 28-if-else-chain/app.ts
@@ -0,0 +1,20 @@
+// Set the value for the variable age
+let age: number = 35;
+
+// Determine the stage of life based on age
+if (age <= 2) {
+    console.log("The person is a baby.");
+} else if (age <= 4) {
+    console.log("The person is a toddler.");
+} else if (age <= 13) {
+    console.log("The person is a kid.");
+} else if (age <=20) {
+    console.log("The person is a teenager.");
+} else if (age <= 35) {
+    console.log("The person is an adult.");
+} else if(age > 60){
+    console.log("The person is elderly.")
+} else 
+{
+    console.log("The person is a senior citizen.");
+}
