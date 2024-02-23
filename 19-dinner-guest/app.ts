559351cb235a7efe17c9e2742abe31576a84d089
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-23 19:23:44 +0500

    Add files via upload

---
+++ 19-dinner-guest/app.ts
@@ -0,0 +1,46 @@
+let guest:string[]=["Muhammad javed","Muhammad Bilal", "Usama Ali"];
+let absent_guest=guest[0];
+
+console.log('Absent Guest :'+absent_guest);
+// adding one guest in the list.
+let new_guest:string="Ali Humza";
+guest[0]="Ali Humza";
+console.log('\nUPDATE GUEST LIST IS :\n');
+
+console.log(' \n Mr. '+ absent_guest+ ' is Absent from the Party.\n');
+console.log('\n Good News ! we find a big table so we invite three more guests to join the party\n')
+ // adding three guests.
+guest.unshift('Ahmed');
+guest.splice(2,0,'Akber Ali');
+guest.push('Hassan Iqbal');
+
+console.log( "\nAFTER ADDING NEW MEMBERS THE GUEST LIST IS:\n" +guest);
+
+// for showing elements of array using loop
+for(var i=0; i<guest.length; i++) {
+    console.log('\n Dear Mr.'+guest[i]+ '\nIt is our Pleasure to invite you our party. \n Thank you \n')
+}
+console.log('\n Sorry! we can not arrange a big table, only two people will be invited.\n');
+// 
+while(guest.length>2) {
+    let removed_guest=guest.pop();
+    console.log('\nSorry Mr.'+removed_guest+',You are not invited for dinner.\n');
+}
+// only two guests remaining. 
+for(var i=0; i<guest.length; i++) {
+    console.log('\n Dear Mr.'+guest[i]+ '\n,You are still invited for dinner. \n Thank you \n');
+}
+// all guests removed from  list.
+guest.splice(0,2);
+console.log(guest);
+
+// Now the list of guests array is empty.
+
+//exercise no    ....   19    ................
+
+
+console.log('remaining number of guests :'+guest.length);
+
+
+
+
