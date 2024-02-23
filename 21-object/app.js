8623eff7487f9f0797673d5f6d9cdd8026ea61fa
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-02-23 19:36:29 +0500

    Add files via upload

---
+++ 21-object/app.js
@@ -0,0 +1,23 @@
+// They think of something you could store in a TypeScript Object.
+//  Write a program that creates Objects containing these items.
+var students = {
+    student1: {
+        name: 'ali',
+        age: 21,
+        roll_number: 1,
+        marks: 95
+    },
+    student2: {
+        name: 'javed',
+        age: 25,
+        roll_number: 2,
+        marks: 98
+    },
+    student3: {
+        name: 'amna',
+        age: 15,
+        roll_number: 3,
+        marks: 100
+    },
+};
+console.log(students);
