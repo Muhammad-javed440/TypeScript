792ebc6163cf3e610fccfae9b8b52502d7966104
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-07 22:57:10 +0500

    Add files via upload

---
+++ 31-empty-array/app.js
@@ -0,0 +1,13 @@
+var fruits = ['apple', 'orange', 'banana', 'grape',];
+console.log(fruits);
+var fruit = fruits.shift();
+// remove the first element of array and return
+console.log(fruit);
+var fruit1 = fruits.shift();
+console.log(fruit1);
+var fruit2 = fruits.shift();
+console.log(fruit2);
+var fruit3 = fruits.shift();
+console.log(fruit3);
+console.log(fruits);
+console.log('we need some more fruits.');
---
+++ 31-empty-array/app.ts
@@ -0,0 +1,14 @@
+let fruits:string[]=['apple', 'orange', 'banana', 'grape',];
+console.log(fruits);
+let fruit=fruits.shift();
+   // remove the first element of array and return
+console.log(fruit);
+let fruit1=fruits.shift(); 
+console.log(fruit1);
+let fruit2=fruits.shift(); 
+console.log(fruit2);
+let fruit3=fruits.shift(); 
+console.log(fruit3);
+console.log(fruits);
+
+   console.log('we need some more fruits.')
