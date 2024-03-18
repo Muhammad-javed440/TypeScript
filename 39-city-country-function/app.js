3f6bade813e08273586b87d813ae82cdbb938924
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 12:23:43 +0500

    Add files via upload

---
+++ 39-city-country-function/app.js
@@ -0,0 +1,7 @@
+function describe_city(city, country) {
+    console.log("\n\"".concat(city, " , ").concat(country, "\"\n"));
+}
+// cities with  country name
+describe_city("Lahore", "pakistan");
+describe_city("Tokyo", "Japan");
+describe_city("New dehli", "India");
---
+++ 39-city-country-function/app.ts
@@ -0,0 +1,7 @@
+function  describe_city(city: string,country: string):void{
+    console.log(`\n"${city} , ${country}"\n`);
+}
+// cities with  country name
+describe_city("Lahore","pakistan");
+describe_city("Tokyo", "Japan");
+describe_city("New dehli", "India");
