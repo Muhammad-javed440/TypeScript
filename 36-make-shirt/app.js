ba468e2f96d7142d2a7cd377f4df15cabdad53bc
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 12:12:59 +0500

    Add files via upload

---
+++ 36-make-shirt/app.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("shirt size: ".concat(size, ", message on shirt: ").concat(message));
+}
+// call function
+make_shirt("Medium", "Hello World");
---
+++ 36-make-shirt/app.ts
@@ -0,0 +1,5 @@
+function make_shirt(size:string,message:string):void {
+    console.log(`shirt size: ${size}, message on shirt: ${message}`);
+}
+// call function
+make_shirt("Medium","Hello World");
