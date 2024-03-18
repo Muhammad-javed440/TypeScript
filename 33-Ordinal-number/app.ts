2d960222aa84a2e19173e3119c03ab45ff2c3281
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 10:45:27 +0500

    Add files via upload

---
+++ 33-Ordinal-number/app.ts
@@ -0,0 +1,13 @@
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const number of numbers) {
+    if (number === 1) {
+        console.log(`${number}st`);
+    } else if (number === 2) {
+        console.log(`${number}nd`);
+    } else if (number === 3) {
+        console.log(`${number}rd`);
+    } else {
+        console.log(`${number}th`);
+    }
+}
