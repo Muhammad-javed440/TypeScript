9eb03aed5a986e56653ade04d69285988013f6de
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 11:37:46 +0500

    Add files via upload

---
+++ 35-common-pet/app.ts
@@ -0,0 +1,8 @@
+const favoritePizzas: string[] = ["Margherita", "Pepperoni", "BBQ Chicken"];
+
+for (const pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log("Margherita, Pepperoni, and BBQ Chicken are some of my favorite pizzas.");
+console.log("I really enjoy exploring different flavors and combinations!");
+console.log("I really love pizza!");
