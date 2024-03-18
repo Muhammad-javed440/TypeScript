add169159a20ebe310a91c705b63083eae370174
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 10:51:18 +0500

    Add files via upload

---
+++ 34-favourit-pizza/app.js
@@ -0,0 +1,8 @@
+var favoritePizzas = ["Margherita", "Pepperoni", "BBQ Chicken"];
+for (var _i = 0, favoritePizzas_1 = favoritePizzas; _i < favoritePizzas_1.length; _i++) {
+    var pizza = favoritePizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("Margherita, Pepperoni, and BBQ Chicken are some of my favorite pizzas.");
+console.log("I really enjoy exploring different flavors and combinations!");
+console.log("I really love pizza!");
