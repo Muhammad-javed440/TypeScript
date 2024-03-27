b4ad8d379379709ea9ae21ca0663a22ed2a1b3e2
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-28 01:15:35 +0600

    Add files via upload

---
+++ 40-artist-name/app.ts
@@ -0,0 +1,21 @@
+function make_album(artist: string, title: string,tracks?:number){
+const album:{ artist: string, title: string,tracks?:number }= {
+    artist:artist,
+    title:title,
+    tracks:tracks
+}
+if(tracks!== undefined){
+    album.tracks=tracks
+}
+    return album;
+}
+
+// Create three dictionaries representing different albums
+let album1 = make_album("Asim Azhar", "DARD");
+let album2 = make_album("Zara noor", "JHOOM");
+let album3 = make_album("Azaan Shaami", "IK LAMHA",15);
+
+// Print each return value to show that objects are storing the album information correctly
+console.log(album1);
+console.log(album2);
+console.log(album3);
